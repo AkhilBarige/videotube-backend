f643c58a0ae8867bddcf9925cd20c7eff5b6534c
AkhilBarige <akhilbarige21@gmail.com>
2025-10-29 20:44:39 +0530

    app.js final commit

--- src/app.js
+++ src/app.js
@@ -1,37 +1,4 @@
-// import express from "express"
-// import cors from "cors"
-// import healthcheckrouter from "./routes/healthcheck.route.js"
-// import cookieParser from "cookie-parser"
-// import userRouter from "./routes/user.route.js"
-// import dotenv from "dotenv";
-// dotenv.config();
 
-
-
-
-
-// const app = express()
-// app.use(cookieParser())
-
-// app.use(cors(
-//     {
-//         origin: process.env.CORS_ORIGIN,
-//         credentials: true
-//     }
-// ))
-
-// app.use(express.json({ limit: "16kb" }))
-
-// app.use(express.urlencoded({ extended: true, limit: "16kb" }))
-// app.use(express.static("public"))
-
-
-// app.use("/api/v1/healthcheck", healthcheckrouter)
-// app.use("/api/v1/users", userRouter)
-
-
-
-// export { app }
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
